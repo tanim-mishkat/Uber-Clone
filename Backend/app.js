702532d18e19a05c610695be6352b1792a121cd9
tanim-mishkat <t5mishkat@gmail.com>
2025-08-25 19:07:53 +0600

    .

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.use("/users", userRoutes);
